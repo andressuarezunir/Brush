bd1ca1a1d46bdae906b642fcf10e8e3ce0749d12
Andrés Suárez <andresfelipe.suarez154@comunidadunir.net>
2024-05-19 13:56:55 -0500

    Missing patch request to update painter added

--- src/app/api/painter/route.ts
+++ src/app/api/painter/route.ts
@@ -1,3 +1,4 @@
+import { uploadImage } from '@/app/helpers';
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
@@ -8,6 +9,46 @@ export async function GET(request: Request) {
   const user = await prisma.painter.findUnique({ where: { id } });
   if (user) return NextResponse.json(user);
   else {
-    return NextResponse.json({ message: 'Painter not found' }, { status: 400 });
+    return NextResponse.json(
+      { error_message: 'Painter not found' },
+      { status: 400 }
+    );
+  }
+}
+
+export async function PATCH(request: Request) {
+  const data = await request.formData();
+
+  const id = data.get('painter_id') as string;
+  const name = data.get('name') as string;
+  const last_name = data.get('last_name') as string;
+  const image = data.get('image') as File;
+  const welcome_message = data.get('welcome_message') as string;
+  const description = data.get('description') as string;
+
+  let image_url;
+  if (image) {
+    image_url = await uploadImage(image);
+  }
+
+  let dataUpdated = {
+    name,
+    last_name,
+    welcome_message,
+    description
+  };
+  if (image_url) dataUpdated = { ...dataUpdated, image: image_url };
+
+  try {
+    const painterUpdated = await prisma.painter.update({
+      where: { id: Number(id) },
+      data: dataUpdated
+    });
+    return NextResponse.json(painterUpdated);
+  } catch (error) {
+    return NextResponse.json(
+      { error_message: 'Painter not updated' },
+      { status: 400 }
+    );
   }
 }
